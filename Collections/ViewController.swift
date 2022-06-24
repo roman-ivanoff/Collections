aa8f550b1a57097d1985c0ec7cb00af9e19b4071
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-06-24 13:02:26 +0300

    Initial Commit

---
+++ Collections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collections
+//
+//  Created by Roman Ivanov on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
