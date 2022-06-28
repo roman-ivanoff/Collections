21f72e67dbbd8350b9739381143e6438e8253f89
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-06-28 21:20:28 +0300

    add viewController logic

--- Collections/Controllers/SetViewController.swift
+++ Collections/Controllers/SetViewController.swift
@@ -8,11 +8,51 @@
 import UIKit
 
 class SetViewController: UIViewController {
+    var model: SetModel? = nil
+    let textFieldModel = TextFieldModel()
+    var navigationItemNumber: Int!
+    let regex = "[A-Za-z]"
+    private lazy var onlyCharactersDelegateObject = InputRuleTextFieldDelegate(rule: OnlyCharactersRule(
+        model: textFieldModel, regex: regex))
 
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var allMatchingLettersLabel: UILabel!
+    @IBOutlet weak var charactersThatDoNotMatchLabel: UILabel!
+    @IBOutlet weak var uniqueCharactersLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        self.title = "Set: \(navigationItemNumber!)"
+
+        firstTextField.delegate = onlyCharactersDelegateObject
+        secondTextField.delegate = onlyCharactersDelegateObject
+    }
+
+    @IBAction func getAllMatchingLettersAction(_ sender: UIButton) {
+        allMatchingLettersLabel.text = model?.getAllMatchingLetters(firstTextField.text!, secondTextField.text!)
+
+        allMatchingLettersLabel.isHidden = false
+    }
+
+    @IBAction func getAllCharactersThatDoNotMatchAction(_ sender: UIButton) {
+        charactersThatDoNotMatchLabel.text = model?.getAllDifferentLetters(firstTextField.text!, secondTextField.text!)
+
+        charactersThatDoNotMatchLabel.isHidden = false
+    }
+
+    @IBAction func getAllUniqueCharactersAction(_ sender: UIButton) {
+        charactersThatDoNotMatchLabel.text = model?.getAllDifferentLetters(firstTextField.text!, secondTextField.text!)
+
+        charactersThatDoNotMatchLabel.isHidden = false
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+
+        view.endEditing(true)
     }
 
 }
