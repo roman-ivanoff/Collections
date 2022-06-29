34366fef40bc3d45eca5acf22d001fd82d5d5fb3
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-06-29 11:11:22 +0300

    added custom cell

---
+++ Collections/Views/CustomCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.swift
+//  Collections
+//
+//  Created by Roman Ivanov on 29.06.2022.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var labelCell: UILabel!
+    @IBOutlet weak var viewCell: UIView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+}
