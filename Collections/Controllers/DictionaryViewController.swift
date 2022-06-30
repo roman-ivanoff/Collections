332d0c42c05c1141e1fc814c7a655952f8b68469
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-06-30 11:00:33 +0300

    added DictionaryVC

---
+++ Collections/Controllers/DictionaryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DictionaryViewController.swift
+//  Collections
+//
+//  Created by Roman Ivanov on 30.06.2022.
+//
+
+import UIKit
+
+class DictionaryViewController: UIViewController {
+    var navigationItemNumber: Int!
+    var model: DictionaryModel!
+    let numberOfSections = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
